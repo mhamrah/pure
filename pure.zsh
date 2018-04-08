70608dd91298bb62dc6c59ae94f0c382fcf825e5
Michael Hamrah <m@hamrah.com>
2018-04-07 21:43:59 -0400

    temporary change as 241 doesn't work with wsl/xterm.js

--- pure.zsh
+++ pure.zsh
@@ -102,7 +102,8 @@ prompt_pure_preprompt_render() {
 
 	# Set color for git branch/dirty status, change color if dirty checking has
 	# been delayed.
-	local git_color=242
+	# temporary change as 242 doesn't work with wsl/xterm.js
+	local git_color=71 #242
 	[[ -n ${prompt_pure_git_last_dirty_check_timestamp+x} ]] && git_color=red
 
 	# Initialize the preprompt array.
